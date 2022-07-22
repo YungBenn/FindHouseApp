c29fef531d2d191cf1b15af658fbddf8dc1f6b15
Ruben Adisuryo <67088252+yungbenn@users.noreply.github.com>
2022-07-22 18:55:41 +0700

    add more popular cities

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -103,6 +103,37 @@ class HomePage extends StatelessWidget {
                   SizedBox(
                     width: 20,
                   ),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Palembang',
+                      imageUrl: 'assets/images/city4.png',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Aceh',
+                      imageUrl: 'assets/images/city5.png',
+                      isPopuler: true,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Bogor',
+                      imageUrl: 'assets/images/city6.png',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
                 ],
               ),
             ),
